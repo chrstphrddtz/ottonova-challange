5187b601ea2adc7e2c49d8cc0f86b97644b85c5d
christoph.raddatz <hello@chrstphrddtz.com>
2023-06-28 21:14:07 +0300

    add Form functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,7 @@ import { use, useState } from "react";
 
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { FormControl, Stack, FormLabel, FormHelperText } from "@mui/material";
-import MuiPhoneNumber from "mui-phone-number";
+import { FormControl, Stack } from "@mui/material";
 
 export default function Form() {
   const [firstName, setFirstName] = useState("");
@@ -15,30 +14,19 @@ export default function Form() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    // alert("Form Submitted");
+    alert("Daten erfolgreich übermittelt");
 
-    const formElement = event.target.elements;
-
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData);
-
-    console.log(event.target);
-    // console.log(formElement.firstName.value);
-    console.log(data);
+    console.log(firstName, lastName, email, phoneNumber);
 
     event.target.reset();
   }
 
   return (
-    <form
-      autoComplete="off"
-      onSubmit={handleSubmit}
-      className="flex flex-col gap-2"
-    >
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
       <h2 className="font-4xl font-semibold">Kontaktformular</h2>
       <Stack className="flex flex-row gap-2">
         <TextField
-          onChange={(e) => setFirstName(e.target.firstName)}
+          onChange={(e) => setFirstName(e.target.value)}
           label="Vorname"
           id="firstName"
           name="firstName"
@@ -49,7 +37,7 @@ export default function Form() {
         />
 
         <TextField
-          onChange={(e) => setLastName(e.target)}
+          onChange={(e) => setLastName(e.target.value)}
           label="Nachname"
           id="lastName"
           name="lastName"
@@ -61,7 +49,7 @@ export default function Form() {
       </Stack>
 
       <TextField
-        onChange={(e) => setEmail(e.target)}
+        onChange={(e) => setEmail(e.target.value)}
         label="Email"
         id="email"
         name="email"
@@ -72,7 +60,7 @@ export default function Form() {
       />
 
       <TextField
-        onChange={(e) => setPhoneNumber(e.target)}
+        onChange={(e) => setPhoneNumber(e.target.value)}
         label="Telefon"
         id="phoneNumber"
         name="phoneNumber"
@@ -81,17 +69,6 @@ export default function Form() {
         size="small"
         required
       />
-
-      {/* <MuiPhoneNumber
-        onChange={(e) => setPhoneNumber(e.target)}
-        defaultCountry={"de"}
-        label="Telefon"
-        id="phoneNumber"
-        name="phoneNumber"
-        variant="outlined"
-        size="small"
-        required
-      /> */}
 
       <Button
         type="submit"
