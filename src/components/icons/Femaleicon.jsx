8c9fb99029aef339e33cf0fbb65bef33dffce273
christoph.raddatz <hello@chrstphrddtz.com>
2023-07-04 14:23:45 +0200

    prettier @ Maleicon & Femaleicon

--- src/components/icons/Femaleicon.jsx
+++ src/components/icons/Femaleicon.jsx
@@ -1,16 +1,15 @@
 import "./Femaleicon.css";
 
 export default function FemaleIcon() {
-
   return (
     <>
       <div className="femaleBody">
-        <div className="femaleHead"/>
-        <div className="femaleDownerBody"/>
-        <div className="femaleLeftLeg"/>
-        <div className="femaleRightLeg"/>
-        <div className="femaleLeftArm"/>
-        <div id="femaleRightArm" className="femaleRightArm"/>
+        <div className="femaleHead" />
+        <div className="femaleDownerBody" />
+        <div className="femaleLeftLeg" />
+        <div className="femaleRightLeg" />
+        <div className="femaleLeftArm" />
+        <div id="femaleRightArm" className="femaleRightArm" />
       </div>
     </>
   );
--- src/components/icons/Maleicon.jsx
+++ src/components/icons/Maleicon.jsx
@@ -1,17 +1,14 @@
 import "./Maleicon.css";
 
 export default function MaleIcon() {
-
-
-
   return (
     <>
       <div className="maleBody">
-        <div className="maleHead"/>
-        <div className="maleLeftLeg"/>
-        <div className="maleRightLeg"/>
-        <div className="maleLeftArm"/>
-        <div id="maleRightArm" className="maleRightArm"/>
+        <div className="maleHead" />
+        <div className="maleLeftLeg" />
+        <div className="maleRightLeg" />
+        <div className="maleLeftArm" />
+        <div id="maleRightArm" className="maleRightArm" />
       </div>
     </>
   );
