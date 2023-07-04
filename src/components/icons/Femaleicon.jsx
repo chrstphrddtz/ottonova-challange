24b4cfebfe2e94fcab7c940e9ee7ed6b0115148a
christoph.raddatz <hello@chrstphrddtz.com>
2023-07-04 14:03:43 +0200

    add male- & female icon + respective css

---
+++ src/components/icons/Femaleicon.jsx
@@ -0,0 +1,17 @@
+import "./Femaleicon.css";
+
+export default function FemaleIcon() {
+
+  return (
+    <>
+      <div className="femaleBody">
+        <div className="femaleHead"/>
+        <div className="femaleDownerBody"/>
+        <div className="femaleLeftLeg"/>
+        <div className="femaleRightLeg"/>
+        <div className="femaleLeftArm"/>
+        <div id="femaleRightArm" className="femaleRightArm"/>
+      </div>
+    </>
+  );
+}
---
+++ src/components/icons/Maleicon.jsx
@@ -0,0 +1,18 @@
+import "./Maleicon.css";
+
+export default function MaleIcon() {
+
+
+
+  return (
+    <>
+      <div className="maleBody">
+        <div className="maleHead"/>
+        <div className="maleLeftLeg"/>
+        <div className="maleRightLeg"/>
+        <div className="maleLeftArm"/>
+        <div id="maleRightArm" className="maleRightArm"/>
+      </div>
+    </>
+  );
+}
